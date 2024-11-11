22fb4b1c76d3b633d304648985f4aa8760b39fb7
sidhp1998 <moitreya.chaudhury@gmail.com>
2024-11-11 23:17:54 +0530

    Walk in sceleton

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  
+  http = inject(HttpClient);
   title = 'MyApp';
+  users:any;
+  ngOnInit():void {
+    this.http.get('http://localhost:5082/api/User/AllUsers').subscribe(
+      {
+        next: response => this.users=response,
+        error: error => console.log(error),
+        complete: () => console.log('request completed')
+      }
+    )
+  }
 }
